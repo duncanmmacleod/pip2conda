cef86047fa2fd802d4f39b569602958b44f4bf62
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-08-31 16:53:55 +0100

    Rebuild docs with more information, better examples, and changelog

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ from pip2conda import __version__ as pip2conda_version
 
 # General information about the project.
 project = "pip2conda"
-copyright = "2022, Cardiff University"
+copyright = "2022-2025, Cardiff University"
 author = "Duncan Macleod"
 version = re.split(r"[\w-]", pip2conda_version)[0]
 release = pip2conda_version
@@ -25,11 +25,47 @@ source_suffix = {
 # extra options
 default_role = "obj"
 
-# html options
-html_theme = "sphinx_rtd_theme"
+# -- HTML formatting --------
+
+html_theme = "furo"
+html_title = f"{project} {version}"
+html_css_files = [
+    # add fontawesome for icons
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/brands.min.css",
+]
+html_theme_options = {
+    # add gitlab link in footer
+    "footer_icons": [
+        {
+            "name": "GitLab",
+            "url": "https://gitlab.com/gwpy/pip2conda",
+            "class": "fa-brands fa-gitlab",
+        },
+    ],
+    # colours
+    "dark_css_variables": {
+        "color-brand-content": (gwpy_dark_mode_colour := "#ededed"),
+        "color-brand-primary": gwpy_dark_mode_colour,
+        "color-brand-visited": gwpy_dark_mode_colour,
+    },
+    "light_css_variables": {
+        "color-brand-content": (gwpy_light_mode_colour := "#29435f"),
+        "color-brand-primary": gwpy_light_mode_colour,
+        "color-brand-visited": gwpy_light_mode_colour,
+    },
+}
+
 
 # -- extensions
 
 extensions = [
     "myst_parser",
+    "sphinx_copybutton",
+]
+
+# myst_parser
+myst_enable_extensions = [
+    "attrs_block",
 ]
---
+++ docs/installation.rst
@@ -0,0 +1,28 @@
+############
+Installation
+############
+
+pip2conda can be installed from conda-forge or PyPI.
+
+From conda-forge (recommended)
+===============================
+
+For best results, please install from `conda-forge <https://conda-forge.org/>`_:
+
+.. code-block:: shell
+
+   conda install -c conda-forge pip2conda
+
+From PyPI
+=========
+
+You can also install directly from PyPI:
+
+.. code-block:: shell
+
+   python -m pip install pip2conda
+
+Requirements
+============
+
+pip2conda supports Python 3.10 and later versions.
